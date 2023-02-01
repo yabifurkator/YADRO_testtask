5d26d15785f910f7e81e2a40884f72c5d701bc1c
Leonid <yabifurkator@scalc.Dlink>
2023-02-01 22:50:25 +0300

    master
    table processing complete

--- tools/common.go
+++ tools/common.go
@@ -1,13 +1,24 @@
 package mycsv
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 const Space = " "
 const EmptyString = ""
 const ColumnNameRegexpr = "[a-zA-Z]+"
 const RowNumberRegexpr = "[0-9]+"
-const OperandRegexpr = "(" + ColumnNameRegexpr + RowNumberRegexpr + ")"
+const CellPosRegexpr = "(" + ColumnNameRegexpr + RowNumberRegexpr + ")"
+const IntegerRegexpr = "([-+]?[0-9]+)"
+const OperandRegexpr = "(" + CellPosRegexpr + "|" + IntegerRegexpr + ")"
+const OperationRegexpr = "[\\+\\-\\*\\/]"
 
 func RemoveSpaces(str *string) (string) {
 	return strings.ReplaceAll(*str, Space, EmptyString)
+}
+
+func IsInt(str *string) bool {
+	_, err := strconv.Atoi(*str);
+	return err == nil;
 }
--- tools/processing.go
+++ tools/processing.go
@@ -44,116 +44,79 @@ func validateKeys(table *Table) (bool, error) {
 	return true, nil
 }
 
-// func validateCells(table *Table) (bool, error) {
-// 	isInt := func (str *string) bool {
-// 		_, err := strconv.Atoi(*str);
-// 		return err == nil;
-// 	}
-
-// 	isExpression := func (str *string) bool {
-// 		operandRegexpr := "(" + ColumnNameRegexpr + RowNumberRegexpr + ")"
-// 		exprRegexpr := "^=" + operandRegexpr + "[\\+\\-\\*\\/]" + operandRegexpr + "$"
-// 		expressionValidator := regexp.MustCompile(exprRegexpr);
-		
-// 		return expressionValidator.MatchString(*str)
-// 	}
-
-// 	for _, colKey := range table.colKeys {
-// 		for _, rowKey := range table.rowKeys {
-// 			cell := table.columns[colKey][rowKey];
-// 			if cell == EmptyString {
-// 				return false, fmt.Errorf(
-// 					("cell can't be empty" +
-// 					"\ncolumn name: %s, row number: %s"),
-// 					colKey,
-// 					rowKey,
-// 				)
-// 			}
-// 			if (!isInt(&cell) && !isExpression(&cell)) {
-// 				return false, fmt.Errorf(
-// 					("cell must be an integer or an expression " +
-// 					"with correct syntax\ncolumn name: %s, row numer: %s"),
-// 					colKey,
-// 					rowKey,
-// 				);
-// 			}
-// 		}
-// 	}
-// 	return true, nil;
-// }
-
-// func validateTable(table *Table) (bool, error) {
-// 	isKeysValid, err := validateKeys(table)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	isCellsValid, err := validateCells(table)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	return (isKeysValid && isCellsValid), nil
-// }
-
-// func ProcessTable(table *Table) (*string, error) {
-// 	_, err := validateTable(table);
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	tbStr := "table_str"
-// 	return &tbStr, nil
-// }
-
-func processExpression(expression *string, table *Table) (string, error) {
+func processExpression(expression *string, table *Table) (*string, error) {
 	operandExpr := regexp.MustCompile(OperandRegexpr)
 	operands := operandExpr.FindAllString(*expression, 2)
 
-	type cellPos struct {
-		colKey string
-		rowKey string
-	}
-	getRowColFromOperand := func (operand *string) (*cellPos) {
-		pos := new(cellPos)
-		pos.colKey = regexp.MustCompile(ColumnNameRegexpr).FindString(*operand)
-		pos.rowKey = regexp.MustCompile(RowNumberRegexpr).FindString(*operand)
-		return pos
-	}
-	operand1 := getRowColFromOperand(&operands[0])
-	operand2 := getRowColFromOperand(&operands[1])
+	operandToInt := func (operand *string) (*int, error) {
+		value, err := strconv.Atoi(*operand)
+		if err == nil {
+			return &value, nil
+		}
+		
+		colKey := regexp.MustCompile(ColumnNameRegexpr).FindString(*operand)
+		rowKey := regexp.MustCompile(RowNumberRegexpr).FindString(*operand)
+		
+		var ok bool
+		_, ok = table.columns[colKey]; if !ok {
+			return nil, fmt.Errorf(
+				"invalid expression \"" + *expression + "\"" +
+				"column \"" + colKey + "\"doesn't exist",
+			)
+		}
+		_, ok = table.columns[colKey][rowKey]; if !ok {
+			return nil, fmt.Errorf(	
+				"invalid expression \"" + *expression + "\"" +
+				"row \"" + rowKey + "\" doesn't exist",
+			)
+		}
 
-	var ok bool
-	_, ok = table.columns[operand1.colKey]; if !ok {
-		return 
-	}
-	_, ok = table.columns[operand1.colKey][operand1.rowKey]; if !ok {
+		cell := table.columns[colKey][rowKey]
+		value, err = strconv.Atoi(cell)
+		if err != nil {
+			fmt.Println(err)
+			return nil, fmt.Errorf(
+				"invalid operand \"" + *operand +
+				"\" in expression \"" + *expression + "\"\n" +
+				"not integer",
+			)
+		}
 
+		return &value, nil
 	}
 
-	fmt.Println("op1:", operand1, "op2:", operand2)
-	// aboba := strconv.FormatFloat()
-	// operand1 := 1.0
-	// operand2 := 2.0
-	// operation := "/"
-	// var result float64
-	// switch operation {
-	// case "+": result = float64(operand1) + float64(operand2)
-	// case "-": result = float64(operand1) - float64(operand2)
-	// case "*": result = float64(operand1) * float64(operand2)
-	// case "/": result = float64(operand1) / float64(operand2)
-	// }
-	return *expression, nil
+	operand1, err := operandToInt(&operands[0])
+	if err != nil {
+		return nil, err
+	}
+	operand2, err := operandToInt(&operands[1])
+	if err != nil {
+		return nil, err
+	}
+	operationStr := regexp.MustCompile(OperationRegexpr).FindString(*expression)
+
+	var result int
+
+	switch operationStr {
+	case "+": result = *operand1 + *operand2
+	case "-": result = *operand1 - *operand2
+	case "*": result = *operand1 * *operand2
+	case "/": result = *operand1 / *operand2
+	}
+
+	// resultStr := strconv.FormatFloat(result, 'f', 2, 64)	
+	resultStr := strconv.Itoa(result)
+	return &resultStr, nil
 }
 
 func ProcessTable(table Table) (*Table, error) {
-	isInt := func (str *string) bool {
-		_, err := strconv.Atoi(*str);
-		return err == nil;
+	_, err := validateKeys(&table)
+	if err != nil {
+		return nil, err
 	}
 
 	isExpression := func (str *string) bool {
-		exprRegexpr := "^=" + OperandRegexpr + "[\\+\\-\\*\\/]" + OperandRegexpr + "$"
+		exprRegexpr := "^=" + OperandRegexpr + OperationRegexpr + OperandRegexpr + "$"
 		expressionValidator := regexp.MustCompile(exprRegexpr);
 		
 		return expressionValidator.MatchString(*str)
@@ -170,7 +133,7 @@ func ProcessTable(table Table) (*Table, error) {
 					rowKey,
 				)
 			}
-			if !isInt(&cell) {
+			if !IsInt(&cell) {
 				if !isExpression(&cell) {
 					return nil, fmt.Errorf(
 						("cell must be an integer or an expression " +
@@ -183,7 +146,7 @@ func ProcessTable(table Table) (*Table, error) {
 				if err != nil {
 					return nil, err
 				}
-				table.columns[colKey][rowKey] = newCell
+				table.columns[colKey][rowKey] = *newCell
 			}
 		}
 	}
